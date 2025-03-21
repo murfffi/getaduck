383d37aaa12fc41b904db9bd410cf023ca6b65f2
Marin Nozhchev <murfffi@gmail.com>
2025-03-21 18:44:35 +0200

    feat: CLI parameters
    Allow configuring what to download with CLI parameters.

--- download/download_test.go
+++ download/download_test.go
@@ -3,14 +3,18 @@ package download_test
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
 	"github.com/murfffi/getaduck/download"
+	"github.com/stretchr/testify/require"
 )
 
 func TestDo(t *testing.T) {
-	res, err := download.Do(download.DefaultSpec())
-	require.NoError(t, err)
-	require.FileExists(t, res.OutputFile)
-
+	if !testing.Short() {
+		t.Skip("skipping test that downloads from Github in short mode.")
+	}
+	t.Run("default lib", func(t *testing.T) {
+		res, err := download.Do(download.DefaultSpec())
+		require.NoError(t, err)
+		require.FileExists(t, res.OutputFile)
+	})
+	// cli is tested e2e in shell/run_test.go . Avoid multiple downloads.
 }
